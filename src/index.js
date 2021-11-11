710444cbafef760db4a76f77f6b9d964fc0cb9ac
SpaceF0X94 <svetyn1994@mail.ru>
2021-11-11 06:25:58 +0000

    меняем цвета

--- src/index.js
+++ src/index.js
@@ -1,60 +1,55 @@
-//1. Придумать структуру данных для хранения списка задач, у каждой задачи есть статус сделана, есть текст и могут быть подзадачи, также с текстом и статусом.* Boolean, objects, array
-
 const tasks = [
-    {
-     text: "Помыть пол",
-     done: true,
-    },
-    {
-     text: "Приготовить пирог",
-     done: false,
-    }            
-  ]
+  {
+    text: "Помыть пол",
+    done: true,
+  },
+  {
+    text: "Приготовить пирог",
+    done: false,
+  }            
+]
+
+const renderTask = ({ text, done }) => `<li data-done="${done}">${text}</li>`
   
+const renderCounter = (tasks) =>
+  `
+    <div class="counter">
+      ${tasks.filter((task) => task.done).length}/${tasks.length}
+    </div>
+  `;
   
-  //отображает наши задачи на странице сайта: task =>string(html)
-  // 1. можно записать как тернарное выражение, кот. обращается к css-классу:
-  //const renderTasks = (task) => `<li class="${task.done ? "class-done" : ""}">${task.text}</li>`;
-  // 2. через data-атрибуты:
-  //const renderTask = (task) => `<li data-done="${task.done}">${task.text}</li>`;
-  // 3. то же, но с деструктуризацией объекта:
-  const renderTask = ({ text, done }) => `<li data-done="${done}">${text}</li>`
-  //сделаем функцию для отображения всех задач и (через дробь)кол-ва выполненных из них:
-  const renderCounter = (tasks) =>
-    `
-      <div class="counter">
-        ${tasks.filter((task) => task.done).length}/${tasks.length}
-      </div>
-    `;
-  
-  //добавляем новую задачу в массив:
   const addTask = (tasks, newTask) => {
    tasks.push(newTask) 
-  }
+}
   
-  //создаём общий render для возможности добавления новых задач
-  const render = () => {
-    const root = document.getElementById("root");
-    const name = "Svetlana";
-    const hello = `<h1>Hi, ${name}!</h1>`;
+const render = () => {
+  const root = document.getElementById("root");
+  const name = "Prokhor";
+  const hello = `<h1>Hi, ${name}!</h1>`;
+
+  const renderedTasks = tasks.map(renderTask).join("");
+  root.innerHTML = `
+    <div>
+      ${hello}
+      ${renderCounter(tasks)}
+      <ul>
+        ${renderedTasks}
+      </ul>
+    </div>
+  `;
+ };
   
-    const renderedTasks = tasks.map(renderTask).join("");
-    root.innerHTML = `
-      <div>
-        ${hello}
-        ${renderCounter(tasks)}
-        <ul>
-          ${renderedTasks}
-        </ul>
-      </div>
-    `;
-  };
-  //общий div нужен для прижатия всего текста по левому краю
+render()
+addTask(tasks, {text: "Сходить в магазин", done: false});
+render()
+
+const colors = ['lightblue', 'LightSalmon', 'Gold', 'PaleGreen', 'plum', 'Aquamarine', 'Bisque', 'DarkSeaGreen']
+let colorIndex = 0
+root.addEventListener('click', () => {
+  const color = colors[colorIndex]
+  colorIndex = (colorIndex + 1) % colors.length
+  root.style = `background-color: ${color};`
+})
+ 
   
-  //вызываем наш рендер с новой задачей:
-  render()
-  addTask(tasks, {text: "Сходить в магазин", done: false});
-  render()
-  
-  // усложнения см. в видео с 2:30
   
